886e6df0a339e77fe71a16ad1581cf87f66bcb59
iVanGB93 <ivanguachbeltran@gmail.com>
2021-07-09 20:27:08 -0400

    update

--- servicios/signals.py
+++ servicios/signals.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import  User
 from servicios.api.serializers import ServiciosSerializer
 from sync.syncs import actualizacion_remota
 from django.core.mail import send_mail
+from decouple import config
 
 @receiver(post_save, sender=User)
 def crearServicios(sender, instance, **kwargs):
@@ -16,37 +17,6 @@ def crearServicios(sender, instance, **kwargs):
         servicios = EstadoServicio(usuario=usuario)
         servicios.sync = True
         servicios.save()
-
-@receiver(post_save, sender=Oper)
-def correoOper(sender, instance, **kwargs):
-    usuario = str(instance.usuario)
-    if instance.servicio != None:
-        servicio = instance.servicio
-    else:
-        servicio = 'None'
-    if instance.codRec != None:
-        codRec = instance.codRec
-    else:
-        codRec = 'None'
-    if instance.haciaDesde != None:
-        haciaDesde = str(instance.haciaDesde)
-    else:
-        haciaDesde = 'None'
-    cantidad = instance.cantidad
-    fecha = str(instance.fecha)
-    data = {'code': instance.code, 'tipo': instance.tipo, 'usuario': usuario, 'servicio': servicio, 'cantidad': instance.cantidad, 'codRec': codRec, 'haciaDesde': haciaDesde, 'fecha': fecha}
-    respuesta = actualizacion_remota('nueva_operacion', data)
-    if not respuesta['estado']:
-        send_mail(f'Falló al subir el servicio', f'La operación de { instance.tipo } del usuario {usuario} no se pudo sincronizar con internet. Fecha: { fecha}.', 'RedCentroHabanaCuba@gmail.com', ['ivanguachbeltran@gmail.com'])    
-    if instance.tipo == 'PAGO':
-        send_mail(f'Pago Realizado -- { usuario }', f'El usuario { usuario } pagó { cantidad } por { servicio }. Fecha: { fecha}', 'RedCentroHabanaCuba@gmail.com', ['ivanguachbeltran@gmail.com'])
-    elif instance.tipo == 'RECARGA':
-        send_mail(f'{ usuario } ha recargado', f'El usuario { usuario } agregó { cantidad } a su cuenta. Código: { instance.codRec }. Fecha: { fecha}', 'RedCentroHabanaCuba@gmail.com', ['ivanguachbeltran@gmail.com'])
-    elif instance.tipo == 'ENVIO':
-        send_mail(f'{ usuario } realizó un envio', f'El usuario { usuario } envió { cantidad } a { instance.haciaDesde }. Fecha: { fecha}', 'RedCentroHabanaCuba@gmail.com', ['ivanguachbeltran@gmail.com'])
-    elif instance == 'RECIBO':
-        send_mail(f'{ usuario } ha recibido', f'El usuario { usuario } recibió { cantidad } de { instance.haciaDesde }. Fecha: { fecha}', 'RedCentroHabanaCuba@gmail.com', ['ivanguachbeltran@gmail.com'])
-
 
 @receiver(post_save, sender=EstadoServicio)
 def actualizar_servicios(sender, instance, **kwargs):
