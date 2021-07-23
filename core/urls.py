25ab085814d0a2a66b205271462f3a3087314595
iVanGB93 <ivanguachbeltran@gmail.com>
2021-07-23 15:51:39 -0400

    update

--- core/urls.py
+++ core/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_done.html"), name="password_reset_complete"),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
