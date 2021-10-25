dc6744fd8d0fb447eaadecb006057c13a0b6cee8
iVanGB93 <ivanguachbeltran@gmail.com>
2021-10-25 15:20:30 -0400

    update

---
+++ chat/views.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+
+from .models import Chat
+
+@login_required(login_url='/users/login/')
+def index(request):
+    content = {'usuarios': User.objects.all()}
+    return render(request, 'chat/index.html', content)
+
+@login_required(login_url='/users/login/')
+def room(request, username=None):
+    if username == None:
+        content = {'usuarios': User.objects.all()}
+        return render(request, 'chat/room.html', content)
+    if User.objects.filter(username=username).exists():
+        usuario2 = User.objects.get(username=username)
+        chats = request.user.chat_set.all()
+        existe = False
+        for chat in chats:
+            if usuario2 in chat.participantes.all():
+                chat = chat
+                existe = True
+                break
+        if not existe:
+            chat = Chat()
+            chat.save()
+            chat.participantes.add(request.user, usuario2)
+        chat_id = chat.id
+        img_url = usuario2.profile.imagen.url
+        if not request.user in chat.participantes.all():
+            return redirect('chat:index')
+        return render(request, 'chat/room.html', {
+            'chat_id': chat_id,
+            'img_url': img_url,
+            'usuarios': User.objects.all()
+        })
+    else:
+        return redirect('chat:index')
