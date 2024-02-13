86f7868a047a4f9d3a2b9e5c4c5c21e4e0d657e4
iVanGB93 <ivanguachbeltran@gmail.com>
2024-02-13 12:45:55 -0500

    latest

--- sync/scheduler.py
+++ sync/scheduler.py
@@ -1,25 +1,30 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from .models import EstadoConexion
+from django.utils import timezone
 import requests
 
 
 def chequeo_conexion_online():
     servidores = EstadoConexion.objects.all()
     for servidor in servidores:
-        print(f"CHEQUEANDO A { servidor.servidor }")
-        url = f'http://{ servidor.ip_cliente }/api/sync/status/{servidor.servidor}/'
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                print("SERVIDOR ONLINE")
-                servidor.online = True
-            else:
+        if servidor.servidor != 'core_ONLINE':
+            print(f"CHEQUEANDO A { servidor.servidor }")
+            url = f'http://{ servidor.ip_cliente }/api/sync/status/{servidor.servidor}/'
+            try:
+                response = requests.get(url)
+                if response.status_code == 200:
+                    print("SERVIDOR ONLINE")
+                    servidor.fecha_chequeo = timezone.now()
+                    servidor.online = True
+                else:
+                    print(f"NO TIENE INTERNET { servidor.servidor }")
+                    servidor.fecha_chequeo = timezone.now()
+                    servidor.online = False
+            except:
                 print(f"NO TIENE INTERNET { servidor.servidor }")
+                servidor.fecha_chequeo = timezone.now()
                 servidor.online = False
-        except:
-            print(f"NO TIENE INTERNET { servidor.servidor }")
-            servidor.online = False
-        servidor.save()
+            servidor.save()
 
 def chequeo_conexiones():
     scheduler = BackgroundScheduler()
